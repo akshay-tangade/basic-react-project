01009eab83dd8395e9b3dcfd7091d8a147914b7e
Akshay Tangade <akshaytangade1999@gmail.com>
2021-10-14 16:02:59 +0530

    project 2 completed

--- Project-2/src/components/UI/Button/Button.js
+++ Project-2/src/components/UI/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import './Button.css';
+import Styles from'./Button.module.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={Styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
